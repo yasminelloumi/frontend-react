f4089769354ebc3cfd1faddc33b879378d681f6a
ZainebAffes <zaineb.affes20@gmail.com>
2024-12-05 11:41:48 +0100

    footer

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -3,11 +3,15 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/footer";
+import "@fortawesome/fontawesome-free/css/all.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Footer />
     </div>
   );
 };
