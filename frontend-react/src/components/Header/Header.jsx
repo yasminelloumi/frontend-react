afcd90267f2b7a08371da5e51b9961f450f2a7e6
yasmine <yelloumi640@gmail.com>
2024-12-05 11:38:08 +0100

    add header

--- frontend-react/src/components/Header/Header.jsx
+++ frontend-react/src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import "./Header.css";
 
 function Header() {
   return (
@@ -9,7 +10,7 @@ function Header() {
         <a href="#">Menu</a>
         <a href="#">Profile</a>
       </ul>
-      
+      {/* Add the user icon here */}
       <div className="header-right">
         <i className="fa-solid fa-user"></i>
       </div>
