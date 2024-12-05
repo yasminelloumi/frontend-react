00440a8183c00dd7629eb60a7fe52790da556c27
yasmine <yelloumi640@gmail.com>
2024-12-05 10:52:03 +0100

    import all

--- frontend-react/src/components/Header/Header.jsx
+++ frontend-react/src/components/Header/Header.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 
-
 function Header() {
   return (
-    <div className="navbar">
-      <img src={logo} alt="Logo" className="logo" /> 
-      <ul className="navbar-menu">
-        <a href="#">Menu</a> 
+    <div className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <ul className="header-menu">
+        <a href="#">Menu</a>
         <a href="#">Profile</a>
       </ul>
+      
+      <div className="header-right">
+        <i className="fa-solid fa-user"></i>
+      </div>
     </div>
   );
 }
