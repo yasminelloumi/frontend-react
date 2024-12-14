07b6ca3a3c2a0e0addf9373508121c550c6e9746
Mohamed Ali Bradai <AL1083647@admin.hubtotal.net>
2024-12-14 15:21:11 +0100

    footer changes

--- frontend-react/src/components/Header/Header.jsx
+++ frontend-react/src/components/Header/Header.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-import logo from "../../assets/logo.png";
+import React from "react";
+import logo from "../../assets/logo.png";  // Ensure the logo path is correct (adjusted from "Logoo.png")
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
-=======
-import React from "react";
-import logo from "../../assets/Logoo.png";
-import "./Header.css";
->>>>>>> 8a00477c78b3a3d4d9519f8bf46bc7833334a9ed
 import { Link } from "react-router-dom";
 
 function Header() {
@@ -15,20 +10,15 @@ function Header() {
       <img src={logo} alt="Logo" className="logo" />
 
       <ul className="header-menu">
-<<<<<<< HEAD
-        <a href="#e-sante">E-Santé</a>  {/* Cible la section E-Santé */}
-        <a href="#a-propos">À Propos</a> {/* Cible la section À Propos */}
-        <a href="#contactez-nous">Contactez-nous</a> {/* Cible la section Contactez-nous */}
-        <Link to="/">Menu</Link>
-        {/* Correct the link to AdminProfile */}
-        <Link to="/AdminProfile">Profile</Link>
-=======
         <Link to="/">HOME</Link>
         <Link to="/Profile">PROFILE</Link>
         <Link to="/ABOUT">ABOUT</Link>
         <Link to="/CONTACT">CONTACT</Link>
->>>>>>> 8a00477c78b3a3d4d9519f8bf46bc7833334a9ed
+        <a href="#e-sante">E-Santé</a> {/* Cible la section E-Santé */}
+        <a href="#a-propos">À Propos</a> {/* Cible la section À Propos */}
+        <a href="#contactez-nous">Contactez-nous</a> {/* Cible la section Contactez-nous */}
       </ul>
+
       <div className="header-right">
         <Link to="/Login">
           <i className="fa-solid fa-user"></i>
