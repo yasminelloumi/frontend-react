6e4edb48d39bf31a3d9de643da81b260dd082a06
yasmine <yelloumi640@gmail.com>
2024-12-19 19:45:58 +0100

    listmedicamen

--- frontend-react/src/components/Medecin/MedecinListe.jsx
+++ frontend-react/src/components/Medecin/MedecinListe.jsx
@@ -1,46 +1,17 @@
-import { useState } from "react";
 import { Table, Container } from "react-bootstrap";
-
 function MedecinListe() {
-  // Hardcoded list of doctors
-  const [medecins] = useState([
-    {
-      Id: 1,
-      Specialite: "Cardiologue",
-      ApplicationUserId: "user123",
-    },
-    {
-      Id: 2,
-      Specialite: "Pédiatre",
-      ApplicationUserId: "user124",
-    },
-    {
-      Id: 3,
-      Specialite: "Dermatologue",
-      ApplicationUserId: "user125",
-    },
-  ]);
-
   return (
     <Container className="mt-5">
       <h2 className="mb-4">Liste des Médecins</h2>
       <Table striped bordered hover responsive>
         <thead>
           <tr>
-            <th>ID</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Password</th>
             <th>Spécialité</th>
-            <th>ID Utilisateur</th>
           </tr>
         </thead>
-        <tbody>
-          {medecins.map((medecin) => (
-            <tr key={medecin.Id}>
-              <td>{medecin.Id}</td>
-              <td>{medecin.Specialite}</td>
-              <td>{medecin.ApplicationUserId}</td>
-            </tr>
-          ))}
-        </tbody>
       </Table>
     </Container>
   );
